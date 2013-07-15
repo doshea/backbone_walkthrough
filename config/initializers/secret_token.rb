a7eeeaa6611612031dc305f2c5c062bbc76bdf29
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-14 22:31:11 -0400

    Initializing Rails 4 project without TestUnit and with Postgres db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = '1498947d4eb98c48d13c1eda997838ef46d9a30f21831320c3aa9ebf76ceccdf15a20fd0256173c96e13297f55eb1a7f089b024be0d8e9462ca425a41de52727'
