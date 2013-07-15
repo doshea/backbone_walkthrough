00a71c1f49d3a2ed1949b3b0e71b1602c850fd29
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-14 22:33:53 -0400

    Adding MainController with index action for our single page app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Raffler::Application.routes.draw do
+  get "main/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
